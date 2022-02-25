e7438e4e8d038a5cb3a262cea618fa95dfa374e3
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2022-02-25 10:23:59 +0100

    Update test action

--- src/test/resources/setup.sh
+++ src/test/resources/setup.sh
@@ -5,7 +5,7 @@ set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker exec -d solr ./mock-proto-server 80
-docker exec -it solr solr create_core -c london
+docker exec solr solr create_core -c london
 curl -X POST -H 'Content-type:application/json' -d '{ "add-field": { "name":"coords", "type":"location", "stored":true } }' http://localhost:8983/solr/london/schema
 curl -X POST -H 'Content-type:application/json' -d '{"add-cache": {"name": "traveltime_fuzzy", "class": "com.traveltime.plugin.solr.cache.FuzzyRequestCache"}}'  http://localhost:8983/solr/london/config
 curl -X POST -H 'Content-type:application/json' -d '{"add-cache": {"name": "traveltime_exact", "class": "com.traveltime.plugin.solr.cache.ExactRequestCache"}}'  http://localhost:8983/solr/london/config
