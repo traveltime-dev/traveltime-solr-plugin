b8d8c27ce3ab0b24834ede0173b186ec2eac4c11
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2022-12-09 09:40:17 +0100

    Add support for solr 9

---
+++ 9/src/main/java/com/traveltime/plugin/solr/query/TraveltimeSearchQuery.java
@@ -0,0 +1,58 @@
+package com.traveltime.plugin.solr.query;
+
+import com.traveltime.plugin.solr.ProtoFetcher;
+import com.traveltime.plugin.solr.cache.RequestCache;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.QueryVisitor;
+import org.apache.solr.search.DelegatingCollector;
+import org.apache.solr.search.ExtendedQueryBase;
+import org.apache.solr.search.PostFilter;
+import org.apache.solr.search.SolrIndexSearcher;
+
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class TraveltimeSearchQuery extends ExtendedQueryBase implements PostFilter {
+   private final TraveltimeQueryParameters params;
+   private final float weight;
+   private final ProtoFetcher fetcher;
+   private final String cacheName;
+
+   @Override
+   public String toString(String field) {
+      return String.format("TraveltimeSearchQuery(params = %s)", params);
+   }
+
+   @Override
+   public void visit(QueryVisitor visitor) {
+      visitor.visitLeaf(this);
+   }
+
+   @Override
+   public DelegatingCollector getFilterCollector(IndexSearcher indexSearcher) {
+      SolrIndexSearcher searcher = (SolrIndexSearcher)indexSearcher;
+      RequestCache cache = (RequestCache) searcher.getCache(cacheName);
+      int maxDoc = searcher.maxDoc();
+      int leafCount = searcher.getTopReaderContext().leaves().size();
+      return new TraveltimeDelegatingCollector(maxDoc, leafCount, params, weight, fetcher, cache);
+   }
+
+   @Override
+   public boolean getCache() {
+      return false;
+   }
+
+   @Override
+   public void setCache(boolean cache) {
+   }
+
+   @Override
+   public int getCost() {
+      return 100;
+   }
+
+   @Override
+   public void setCost(int cost) {
+   }
+}
---
+++ 9/src/main/java/com/traveltime/plugin/solr/query/TraveltimeValueSourceParser.java
@@ -0,0 +1,60 @@
+package com.traveltime.plugin.solr.query;
+
+import com.traveltime.plugin.solr.cache.RequestCache;
+import lombok.val;
+import org.apache.lucene.queries.function.ValueSource;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.search.FunctionQParser;
+import org.apache.solr.search.SyntaxError;
+import org.apache.solr.search.ValueSourceParser;
+
+import static com.traveltime.plugin.solr.TraveltimeQParserPlugin.PARAM_PREFIX;
+
+public class TraveltimeValueSourceParser extends ValueSourceParser {
+   private String cacheName = RequestCache.NAME;
+
+   @Override
+   public void init(NamedList args) {
+      super.init(args);
+      Object cache = args.get("cache");
+      if(cache != null) cacheName = cache.toString();
+   }
+
+   private String getParam(SolrParams params, String name) throws SyntaxError {
+      String param = params.get(PARAM_PREFIX + name);
+      if(param != null) return param;
+      param = params.get(name);
+      if(param != null) return param;
+      throw new SyntaxError("missing " + name + " parameter for TravelTime value source");
+   }
+
+
+   @Override
+   public ValueSource parse(FunctionQParser fp) throws SyntaxError {
+      SolrQueryRequest req = fp.getReq();
+      RequestCache cache = (RequestCache) req.getSearcher().getCache(cacheName);
+      if (cache == null) {
+         throw new SolrException(
+             SolrException.ErrorCode.BAD_REQUEST,
+             "No request cache configured."
+         );
+      }
+
+      SolrParams params = fp.getParams();
+
+
+      val queryParameters = TraveltimeQueryParameters.fromStrings(
+          req.getSchema(),
+          getParam(params, TraveltimeQueryParameters.FIELD),
+          getParam(params, TraveltimeQueryParameters.ORIGIN),
+          getParam(params, TraveltimeQueryParameters.LIMIT),
+          getParam(params, TraveltimeQueryParameters.MODE),
+          getParam(params, TraveltimeQueryParameters.COUNTRY)
+      );
+
+      return new TraveltimeValueSource(queryParameters, cache.getOrFresh(queryParameters));
+   }
+}
