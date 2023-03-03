03f63d6373c53b66a5bf30843a2b8fc670420bc5
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2023-03-03 13:31:39 +0100

    Port refactor and scoring to solr 9

--- 9/src/main/java/com/traveltime/plugin/solr/cache/FuzzyTimeFilterRequestCache.java
+++ 9/src/main/java/com/traveltime/plugin/solr/cache/FuzzyTimeFilterRequestCache.java
@@ -1,23 +1,23 @@
 package com.traveltime.plugin.solr.cache;
 
-import com.traveltime.plugin.solr.query.TraveltimeQueryParameters;
+import com.traveltime.plugin.solr.query.timefilter.TimeFilterQueryParameters;
 import org.apache.solr.search.CacheRegenerator;
 
 import java.util.Map;
 
-public class FuzzyRequestCache extends RequestCache<TraveltimeQueryParameters> {
+public class FuzzyTimeFilterRequestCache extends RequestCache<TimeFilterQueryParameters> {
    private final Object[] lock = new Object[0];
    private Map<String, String> args;
 
    @Override
-   public Object init(Map<String, String> args, Object persistence, CacheRegenerator regenerator) {
+   public Object init(Map args, Object persistence, CacheRegenerator regenerator) {
       this.args = args;
       return super.init(args, persistence, regenerator);
    }
 
    @Override
-   public TravelTimes getOrFresh(TraveltimeQueryParameters key) {
-      key = new TraveltimeQueryParameters(null, key.getOrigin(), 0, key.getMode(), null);
+   public TravelTimes getOrFresh(TimeFilterQueryParameters key) {
+      key = key.withField(null).withTravelTime(0);
       TravelTimes result = get(key);
       if (result == null) {
          synchronized (lock) {
