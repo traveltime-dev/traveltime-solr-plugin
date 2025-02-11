e2020e8bbc28117830f6f40bf5c6684ad6f90467
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2025-02-11 13:03:20 +0100

    Factor out exact caches

--- 6/src/main/java/com/traveltime/plugin/solr/cache/ExactTimeFilterRequestCache.java
+++ 6/src/main/java/com/traveltime/plugin/solr/cache/ExactTimeFilterRequestCache.java
@@ -3,20 +3,11 @@ package com.traveltime.plugin.solr.cache;
 import com.traveltime.plugin.solr.query.timefilter.TimeFilterQueryParameters;
 
 public class ExactTimeFilterRequestCache extends RequestCache<TimeFilterQueryParameters> {
-  private final Object[] lock = new Object[0];
+  private final ExactUnadaptedRequestCache<TimeFilterQueryParameters> unadapted =
+      new ExactUnadaptedRequestCache<>(this::get, this::put);
 
   @Override
   public TravelTimes getOrFresh(TimeFilterQueryParameters key) {
-    TravelTimes result = get(key);
-    if (result == null) {
-      synchronized (lock) {
-        result = get(key);
-        if (result == null) {
-          result = new BasicTravelTimes();
-          put(key, result);
-        }
-      }
-    }
-    return result;
+    return unadapted.getOrFresh(key);
   }
 }
--- 9.0-3/src/main/java/com/traveltime/plugin/solr/cache/ExactRequestCache.java
+++ 9.0-3/src/main/java/com/traveltime/plugin/solr/cache/ExactRequestCache.java
@@ -3,20 +3,11 @@ package com.traveltime.plugin.solr.cache;
 import com.traveltime.plugin.solr.query.TravelTimeQueryParameters;
 
 public class ExactRequestCache extends RequestCache<TravelTimeQueryParameters> {
-  private final Object[] lock = new Object[0];
+  private final ExactUnadaptedRequestCache<TravelTimeQueryParameters> unadapted =
+      new ExactUnadaptedRequestCache<>(this::get, this::put);
 
   @Override
   public TravelTimes getOrFresh(TravelTimeQueryParameters key) {
-    TravelTimes result = get(key);
-    if (result == null) {
-      synchronized (lock) {
-        result = get(key);
-        if (result == null) {
-          result = new BasicTravelTimes();
-          put(key, result);
-        }
-      }
-    }
-    return result;
+    return unadapted.getOrFresh(key);
   }
 }
